92ba9a25021892cc24d32562d7464ba41fd7e503
nguyenquangbinh12 <ltlien78@gmail.com>
2024-08-25 17:10:17 +0700

    Add files via upload

---
+++ cuoikhoa.py
@@ -0,0 +1,9 @@
+ for i in range (1,11):
+  for j in range (1,6):
+        print (j,"x",i,"=",i*j,end='\t')for i in range (1,11):
+print()
+print()
+ for i in range (1,11):
+  for j in range (6,11):
+        print (j,"x",i,"=",i*j,end='\t')for i in range (1,11):
+print()
